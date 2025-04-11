2d7db7cf6535b736cd7696d1d9c9b6320dc0160c
mahesh2291 <gmahesh252525@gmail.com>
2025-04-11 09:17:42 -0400

    exploring get,post,patch,put,delete

--- src/app.js
+++ src/app.js
@@ -2,9 +2,26 @@ const express=require('express');
 
 const app=express()
 
-app.use('/',(req,res)=>{
-    res.send("This is Homepage Test")
+app.get('/user',(req,res)=>{
+    res.send({firstName:"Uma Mahesh",lastName:"Giduthuri"})
 })
+
+app.post('/user',(req,res)=>{
+    res.send("Data saved Successfully")
+})
+
+app.delete('/user',(req,res)=>{
+    res.send("User Deleted successfully")
+})
+
+app.put('/user',(req,res)=>{
+    res.send("put successful")
+})
+
+app.patch('/user',(req,res)=>{
+    res.send("Patch successful")
+})
+
 
 
 app.listen(3000,()=>{
