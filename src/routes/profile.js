91ab8943deb4f5d06e37ed0ef18dcaef1f6c7274
mahesh2291 <gmahesh252525@gmail.com>
2025-04-18 18:27:31 -0400

    logout/edit password Apis

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,9 +1,11 @@
 const express=require('express')
 const { userAuth } = require('../middlewares/auth')
+const {validateEditProfileData}= require('../utils/validateSignup')
+const bcrypt=require('bcrypt')
 
 const profileRouter=express.Router()
 
-profileRouter.get('/profile',userAuth,async(req,res)=>{
+profileRouter.get('/profile/view',userAuth,async(req,res)=>{
     try {
         const user=req.user
     res.send(user)
@@ -12,4 +14,41 @@ profileRouter.get('/profile',userAuth,async(req,res)=>{
 }
 })
 
+profileRouter.patch('/profile/edit',userAuth,async(req,res)=>{
+     try {
+      if(!validateEditProfileData(req)){
+        throw new error("Invalid edit request")
+      }
+      const user=req.user
+
+      Object.keys(req.body).forEach((key)=>(user[key]=req.body[key]))
+
+      await user.save();
+
+      res.send(`${user.firstName} profile updated successfully`)
+
+     } catch (err) {
+        res.status(400).send(err)
+     }
+})
+
+profileRouter.patch('/profile/forgotpassword',userAuth,async(req,res)=>{
+     try {
+        const {oldPassword,newPassword}=req.body
+        const user=req.user
+        const isPasswordValid=await user.validatePasswordHash(oldPassword)
+        if(!isPasswordValid) {
+            throw new Error("Entered old password doesnt match")
+        }
+  
+        const newPasswordToDb=await bcrypt.hash(newPassword,10)
+        user.password=newPasswordToDb
+        console.log(user)
+        await user.save()
+        res.send('Password changed successfully')
+
+     } catch (err) {
+        res.status(400).send(err.message)
+     }
+})
 module.exports=profileRouter
--- src/utils/validateSignup.js
+++ src/utils/validateSignup.js
@@ -9,6 +9,16 @@ const validateSignup=(req)=>{
 else if(age<18) throw new Error("Your age is less than 18")
 }
 
+const validateEditProfileData=(req)=>{
+   const allowedEditFields=["firstName","lastName","email","age","gender","photoUrl","about","skills"]
+        
+  const isEditAllowed=Object.keys(req.body).every(field=>allowedEditFields.includes(field))
+
+ return isEditAllowed
+
+  }
+
 module.exports={
-    validateSignup
+    validateSignup,
+    validateEditProfileData
 }
